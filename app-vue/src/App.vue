0abe7ef732d64b24a42f345305739ff24f4a5e37
joeltok <jttctc@gmail.com>
2024-02-22 06:07:34 +0800

    init vue app with native component

---
+++ app-vue/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+import GroceryList from './components/GroceryList.vue'
+
+const groceryItems = [
+  "eggs",
+  "lettuce",
+  "oat milk",
+]
+
+const hasReminder = ref(false)
+const sendReminder = (val) => {
+  hasReminder.value = val
+}
+</script>
+
+<template>
+  <main>
+    <grocery-list 
+      title="Day 23 supply run"
+      :groceryItems="groceryItems"
+      :hasReminder="hasReminder"
+      @sendReminder="sendReminder"
+    ></grocery-list>
+    <div className="groceries-alert">
+      <p>{{ hasReminder ? 'Please buy groceries ya :)' : '' }}</p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.groceries-alert {
+  height: 20px;
+}
+
+.groceries-alert > p {
+  font-size: 12px;
+}
+</style>
---
+++ app-vue/src/components/GroceryList.vue
@@ -0,0 +1,50 @@
+<script setup>
+const props = defineProps(['title', 'groceryItems', 'hasReminder'])
+const emit = defineEmits(['sendReminder'])
+
+const onReminderClick = () => {
+  emit('sendReminder', !props.hasReminder)
+}
+</script>
+
+<template>
+  <div id="grocery-list">
+    <h2 id="title">{{ title }}</h2>
+    <ul id="grocery-items">
+      <li v-for="item in groceryItems" key="item">
+        {{ item }}
+      </li>
+    </ul>
+    <button @click="onReminderClick">{{ !props.hasReminder ? 'Send Reminder' : 'Cancel Reminder'}}</button>
+  </div>
+</template>
+
+<style>
+#grocery-list {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  background-color: aliceblue;
+  max-width: 150px;
+}
+
+#grocery-list > h2, #grocery-items > li {
+  color: black;
+  margin: 0;
+  padding: 0;
+}
+
+#grocery-list > h2 {
+  font-size: 16px;
+}
+
+#grocery-items {
+  margin: 10px 0;
+  padding: 0 0 0 20px;
+  text-align: left;
+}
+
+#grocery-items > li {
+  font-size: 12px;
+}
+</style>
